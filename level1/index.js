5f9303dc976cbd58fd3f2fefb9409ca71cda63aa
akashmondkar7 <akashmondkar03@gmail.com>
2026-06-16 22:19:31 +0530

    Docker

---
+++ level1/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const port=process.env.PORT || 4000;
+
+const app= express();
+
+app.get("/",(req,resp)=>{
+    return resp.status(200).json({message:"hello from docker"})
+})
+
+app.listen(port,()=>{
+    console.log("Server Started")
+})
