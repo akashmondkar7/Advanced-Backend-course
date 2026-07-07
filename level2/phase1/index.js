98397d25a6d7cf0bb2bf17a4b50b5edf7412aa77
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-07 21:47:00 +0530

    server

---
+++ level2/phase1/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './lib/db.js';
+
+dotenv.config();
+
+const port=process.env.PORT || 4000;
+
+
+
+const app= express();
+
+
+
+
+
+app.get("/",(req,resp)=>{
+    return resp.status(200).json({message:"hello from docker"})
+})
+
+app.listen(port,()=>{
+    connectDB();
+    console.log(`Server Started ${port}`)
+})
+
